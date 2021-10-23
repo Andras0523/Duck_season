21ba18ef72dc9b48e472472abea4acbc3babaf28
Andras0523 <42574841+Andras0523@users.noreply.github.com>
2021-10-23 22:53:39 +0200

    nagyon sok minden

---
+++ Duck Season/Assets/Scripts/ResScale.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResScale : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Screen.SetResolution(1536,768,true);
+    }
+
+
+}
